5e057164e614cbc18be6348a8f2cb4902879e253
dastangoo <hamed.dastangoo@gmail.com>
2017-08-18 09:43:28 +0430

    Using the Revealing Module Pattern - NOTE: The markup and js codes of Canvas Chart aren't complete.

---
+++ Scripts/RevealingModulePattern.js
@@ -0,0 +1,106 @@
+var calculator = function () {
+	var eqCtl,
+		currNumberCtl,
+		operator,
+		operatorSet = false,
+		equalsPressed = false,
+		lastNumber = null,
+			
+		init = function (equals, currNumber) {
+			eqCtl = euqals;
+			currNumberCtl = currNumber;
+		},
+	
+		add = function (x, y) {
+			return x + y;
+		},
+		subtract = function (x, y) {
+			return x - y;
+		},
+			
+		multiply = function (x, y) {
+			return x * y;
+		},
+		divide = function (x, y) {
+			if (y === 0) {
+				document.alert("Can't divide by 0");
+				return 0;
+			}
+			return x / y;
+		},
+		setValue = function (val) {
+			currNumberCtl.innerHTML = val;
+		},
+		setEquation = function (val) {
+			eqCtl.innerHTML = val;
+		},
+		calculate = function () {
+			if (!operator || lastNumber === null) {
+				return;
+			}
+			var currNumber = parseFloat(currNumberCtl.innerHTML),					newVal = 0;
+			//eval() would've made this a whole lot simpler 
+			//but didn't want to use it in favor of a more
+			//"robust" set of methods to demo patterns
+			switch (operator) {
+			case '+':
+				newVal = add(lastNumber, currNumber);
+				break;
+			case '-':
+				newVal = subtract(lastNumber, currNumber);
+				break;
+			case '*':
+				newVal = multiply(lastNumber, currNumber);
+				break;
+			case '/':
+				newVal = divide(lastNumber, currNumber);
+				break;
+			}
+			setValue(newVal);
+			lastNumber = newVal;
+		},
+		clearNumbers = function () {
+			lastNumber = null;
+			equalsPressed = operatorSet = false;
+			setValue('0');
+			setEquation('');
+		},
+		numberClick = function (e) {
+			var button = (e.target) ? e.target : e.srcElement;
+			if (operatorSet === true || currNumberCtl.innerHTML === '0') {
+				setValue('');
+				operatorSet = false;
+			}
+			setValue(currNumberCtl.innerHTML + button.innerHTML);
+			setEquation(eqCtl.innerHTML + button.innerHTML);
+		},
+		setOperator = function (newOperator) {
+		if (newOperator === '=') {
+			equalsPressed = true;
+			calculate();
+			setEquation('');
+			return;
+		}
+		//Handle case where = was pressed
+		//followed by an operator (+,-,*,/)
+		if (!equalsPressed) {
+			calculate();
+		}
+		equalsPressed = false;
+		operator = newOperator;
+		operatorSet = true;
+		lastNumber = parseFloat(currNumberCtl.innerHTML);
+		var eqText = (eqCtl.innerHTML === '') ?
+					lastNumber + ' ' + operator + ' ' :
+					eqCtl.innerHTML + ' ' + operator + ' ';
+	};
+
+
+//public memebers
+	return {
+		init: init,
+		numberClick: numberClick(),
+		setOperator: setOperator,
+		clearNumbers: clearNumbers
+	};
+}();
